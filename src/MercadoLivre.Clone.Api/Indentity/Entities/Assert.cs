090bd2cdab1487d4d1bdbb6abdb608777ef481b9
Fernando Gonçalves <hd1.fernandosouza@hotmail.com>
2022-05-26 20:17:15 -0300

    feat: criação de pré-condições para classe IdentityUserEntity

---
+++ src/MercadoLivre.Clone.Api/Indentity/Entities/Assert.cs
@@ -0,0 +1,10 @@
+﻿namespace MercadoLivre.Clone.Api.Indentity.Entities;
+
+public static class Assert
+{
+    public static void IsTrue(bool value, string message)
+    {
+        if (value == false)
+            throw new InvalidOperationException(message);
+    }
+}
--- src/MercadoLivre.Clone.Api/Indentity/Entities/IdentityUserEntity.cs
+++ src/MercadoLivre.Clone.Api/Indentity/Entities/IdentityUserEntity.cs
@@ -1,32 +1,53 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.Net.Mail;
 
-namespace MercadoLivre.Clone.Api.Indentity.Entities
+namespace MercadoLivre.Clone.Api.Indentity.Entities;
+
+// CI: 2
+public class IdentityUserEntity : IdentityUser
 {
-    public class IdentityUserEntity : IdentityUser
+    public DateTimeOffset RegistrationTime { get; protected set; }
+
+    [Obsolete("apenas para orm")]
+    public IdentityUserEntity()
     {
-        public DateTimeOffset RegistrationTime { get; protected set; }
 
-        [Obsolete("apenas para orm")]
-        public IdentityUserEntity()
+    }
+
+    public IdentityUserEntity(
+        string login,
+        string userName,
+        DateTimeOffset registrationTime,
+        bool emailConfirmed = true)
+    {
+        // 1
+        Assert.IsTrue(IsValidEmail(login), $"{nameof(login)} deve ser um email válido");
+        Assert.IsTrue(IsValidEmail(userName), $"{nameof(userName)} deve ser um email válido");
+        Assert.IsTrue(registrationTime <= DateTimeOffset.UtcNow, $" {nameof(registrationTime)} não pode estar no futuro.");
+
+        Email = login;
+        NormalizedEmail = login.ToUpper();
+        UserName = userName;
+        NormalizedUserName = userName.ToUpper();
+        RegistrationTime = registrationTime;
+        EmailConfirmed = emailConfirmed;
+    }
+
+    private bool IsValidEmail(string email)
+    {
+        try
         {
+            email = email.Trim();
+            // 1
+            if (email.EndsWith("."))
+                return false;
 
+            var addr = new MailAddress(email);
+            return addr.Address == email;
         }
-
-        public IdentityUserEntity(
-            string login,
-            string userName,
-            DateTimeOffset registrationTime,
-            bool emailConfirmed = true)
+        catch
         {
-            if (registrationTime >= DateTimeOffset.UtcNow)
-               throw new ArgumentOutOfRangeException(nameof(registrationTime), "a data de registro do usuário não pode estar no futuro.");
-
-            Email = login;
-            NormalizedEmail = login.ToUpper();
-            UserName = userName;
-            NormalizedUserName = userName.ToUpper();
-            RegistrationTime = registrationTime;
-            EmailConfirmed = emailConfirmed;
+            return false;
         }
     }
 }
