4b60eab0d8b14471a46d8916257b1c53aab25b86
Fernando Gonçalves <hd1.fernandosouza@hotmail.com>
2022-05-31 20:36:26 -0300

    feat: configure data projet with nHibernate

---
+++ src/MercadoLivre.Clone.Data/Repository/UnitOfWOrk.cs
@@ -0,0 +1,13 @@
+﻿using MercadoLivre.Clone.Business.Repository;
+using NHibernate;
+
+namespace MercadoLivre.Clone.Data.Repository;
+public class UnitOfWOrk : IUnitOfWork
+{
+    private static ISessionFactory SessionFactory => null;
+
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+}
