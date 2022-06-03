a1297da73ba4ec83b17a12d97c8f1eba37f2f028
Fernando Gonçalves <hd1.fernandosouza@hotmail.com>
2022-06-03 20:48:47 -0300

    feat: criação de migrations
    foi necessário usar .net devido a erros de execução.
    conforme: https://github.com/fluentmigrator/fluentmigrator/issues/1544

---
+++ src/MercadoLivre.Clone.Data.Migrations/Migrations/M010CreateCategoryTable.cs
@@ -0,0 +1,24 @@
+﻿using FluentMigrator;
+
+namespace MercadoLivre.Clone.Data.Migrations
+{
+    [Migration(2022202442, "Criação da tabela Categoria")]
+    public class M010CreateCategoryTable : Migration
+    {
+        private string TableName = "Category";
+
+        public override void Down()
+        {
+            Delete.Table(TableName);
+        }
+
+        public override void Up()
+        {
+
+            Create.Table(TableName)
+                .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+                .WithColumn("Name").AsString();
+        }
+    }
+}
+//  dotnet-fm migrate -a .\MercadoLivre.Clone.Data.Migrations.dll -p SqlServer2016 -o -t local -c "Server=localhost,1433;DataBase=CasaDoCodigoClone;User Id=sa;Password=P@ssword42;Trusted_Connection=false"     --allowDirtyAssemblies
