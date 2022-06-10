a34e992759c61f2482fd734331c3dc3205db605f
Fernando Gonçalves <hd1.fernandosouza@hotmail.com>
2022-06-09 21:47:41 -0300

    fix: ajustes na validação de categoria não permitindo categorias filhas duplicadas

--- src/MercadoLivre.Clone.Data/Repository/CategoryRepository.cs
+++ src/MercadoLivre.Clone.Data/Repository/CategoryRepository.cs
@@ -3,6 +3,7 @@ using MercadoLivre.Clone.Business.Repository;
 using NHibernate;
 using NHibernate.Linq;
 using System.Data;
+using System.Linq.Expressions;
 
 namespace MercadoLivre.Clone.Data.Repository;
 
@@ -14,8 +15,12 @@ public class CategoryRepository : Repository<CategoryEntity, int>, ICategoryRepo
 
     public async Task<bool> CategoryAlreadyExistAsync(string name, int categoryParentId, CancellationToken cancellationToken)
     {
+        Expression<Func<CategoryEntity, bool>> predicate = categoryParentId == default
+             ? x => x.Name == name
+             : x => x.Name == name && x.Parent.Id == categoryParentId;
+
         var result = await Session.Query<CategoryEntity>()
-            .Where(x => x.Name == name && x.Parent.Id == categoryParentId)
+            .Where(predicate)
             ?.FirstOrDefaultAsync(cancellationToken);
 
         return result is not null;
