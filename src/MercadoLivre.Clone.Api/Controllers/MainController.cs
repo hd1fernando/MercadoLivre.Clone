ce6083eddff5711b8010d2377da60fc3b9e1066c
Fernando Gonçalves <hd1.fernandosouza@hotmail.com>
2022-06-11 20:53:31 -0300

    feat: criação de classe abstrata para gerenciar controllers

---
+++ src/MercadoLivre.Clone.Api/Controllers/MainController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MercadoLivre.Clone.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public abstract class MainController : ControllerBase
+{
+
+}
