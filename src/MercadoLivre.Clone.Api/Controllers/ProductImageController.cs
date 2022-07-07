e63fb8d690680220b47726293f948fd7f08f8f11
Fernando Gonçalves <hd1.fernandosouza@hotmail.com>
2022-07-06 21:25:14 -0300

    :poop: fazer upload de imagem do produto por meio de string base64

---
+++ src/MercadoLivre.Clone.Api/Controllers/ProductImageController.cs
@@ -0,0 +1,46 @@
+﻿using MercadoLivre.Clone.Api.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MercadoLivre.Clone.Api.Controllers;
+
+public class ProductImageController : MainController
+{
+    [HttpPost]
+    public async Task<ActionResult> Update(ProductImageViewModel productImageView)
+    {
+        var imageName = $"{Guid.NewGuid()}_{productImageView.ImageName}";
+        if (UploadFile(productImageView.Image, imageName) == false)
+        {
+            return BadRequest(ModelState);
+        }
+
+        productImageView.ImageName = imageName;
+
+
+        return Ok();
+    }
+
+    private bool UploadFile(string file, string imgName)
+    {
+        var imageDataByteArray = Convert.FromBase64String(file);
+
+        if (string.IsNullOrEmpty(file))
+        {
+            ModelState.AddModelError(string.Empty, "Forneca uma image para esse produto.");
+            return false;
+        }
+
+        var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/imagens", imgName);
+
+        if (System.IO.File.Exists(filePath))
+        {
+            ModelState.AddModelError(string.Empty, "Já existe um arquivo com esse nome.");
+            return false;
+        }
+
+        System.IO.File.WriteAllBytes(filePath, imageDataByteArray);
+
+        return true;
+    }
+
+}
---
+++ src/MercadoLivre.Clone.Api/Dtos/ProductImageViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MercadoLivre.Clone.Api.Dtos;
+
+public class ProductImageViewModel
+{
+    [Required(ErrorMessage = "{0} é obrigatório")]
+    [Range(1, int.MaxValue, ErrorMessage = "{0} deve ter um valor válido")]
+    public int ProductId { get; set; }
+
+    [Required(ErrorMessage = "{0} é obrigatório.")]
+    public string? ImageName { get; set; }
+
+    public string? Image { get; set; }
+}
