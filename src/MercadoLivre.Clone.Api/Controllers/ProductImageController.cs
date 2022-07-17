79ae4ffe597db8a1a83e8c3a273190594b82d593
Fernando Gonçalves <hd1.fernandosouza@hotmail.com>
2022-07-16 22:23:02 -0300

    :coffin: remoção de upload de arquivo da Controller

--- src/MercadoLivre.Clone.Api/Controllers/ProductImageController.cs
+++ src/MercadoLivre.Clone.Api/Controllers/ProductImageController.cs
@@ -33,69 +33,6 @@ public class ProductImageController : MainController
         var productImageCommand = _mapper.Map<ProductImageCommand>(productImageViewModel);
         await _mediator.Send(productImageCommand);
 
-        //foreach (var file in files)
-        //{
-        //    var prefix = $"{Guid.NewGuid()}_";
-        //    string imagePath = "wwwroot/app/demo-webapi/src/assests";
-        //    var path = Path.Combine(Directory.GetCurrentDirectory(), imagePath, prefix + file.FileName);
-
-        //    if (CanUploadFile(file, path) == false)
-        //        return BadRequest(ModelState);
-
-        //    await UploadIFormFileAsync(file, path);
-        //}
-
         return Ok();
     }
-
-
-    private bool CanUploadFile(IFormFile file, string path)
-    {
-        if (string.IsNullOrEmpty(path))
-            throw new ArgumentNullException(path, $"{nameof(path)} deve conter um valor");
-
-        if (file is null || file.Length == 0)
-        {
-            ModelState.AddModelError(string.Empty, "Forneça uma imagem para esse produto");
-            return false;
-        }
-
-        if (System.IO.File.Exists(path))
-        {
-            ModelState.AddModelError(string.Empty, "Já existe um arquivo com esse nome");
-            return false;
-        }
-
-        return true;
-    }
-
-    private async Task UploadIFormFileAsync(IFormFile file, string path)
-    {
-        using var stream = new FileStream(path, FileMode.Create);
-        await file.CopyToAsync(stream);
-    }
-
-    private bool UploadBase64File(string file, string imgName)
-    {
-        var imageDataByteArray = Convert.FromBase64String(file);
-
-        if (string.IsNullOrEmpty(file))
-        {
-            ModelState.AddModelError(string.Empty, "Forneca uma imagem para esse produto.");
-            return false;
-        }
-
-        var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/imagens", imgName);
-
-        if (System.IO.File.Exists(filePath))
-        {
-            ModelState.AddModelError(string.Empty, "Já existe um arquivo com esse nome.");
-            return false;
-        }
-
-        System.IO.File.WriteAllBytes(filePath, imageDataByteArray);
-
-        return true;
-    }
-
 }
